e0442ba6309f382ad720ec385ced6d9b710b2fec
Daniel Werminghausen <daniel.werminghausen@gmail.com>
2021-04-05 22:24:19 -0400

    Revert "Adding material ui and job finder component"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import JobFinder from './components/JobFinder';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <JobFinder />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Cool
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
